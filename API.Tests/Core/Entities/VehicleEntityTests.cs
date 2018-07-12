fa8b8cd2589480a424cd0190f37c170ee6a8630f
Chetan Patel <ch3tanp@gmail.com>
2018-07-12 00:14:50 -0400

    Adding Testing solution
    Testing

---
+++ API.Tests/Core/Entities/VehicleEntityTests.cs
@@ -0,0 +1,11 @@
+﻿using API.Core.Entities;
+using System.Linq;
+using Xunit;
+
+namespace API.Tests.Core.Entities
+{
+    public class VehicleEntityTests
+    {
+       //Test cases for VehicalEntity
+    }
+}
