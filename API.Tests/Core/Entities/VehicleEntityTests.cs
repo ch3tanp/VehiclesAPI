fc8cb532fb76d3aca077dc8b5733b13e122d4fb9
Chetan Patel <ch3tanp@gmail.com>
2018-07-16 14:52:34 -0400

    Adding Entity data annotation tests
    Adding Entity data annotation tests for Vehicle Year Range.

--- API.Tests/Core/Entities/VehicleEntityTests.cs
+++ API.Tests/Core/Entities/VehicleEntityTests.cs
@@ -1,4 +1,6 @@
 ﻿using API.Core.Entities;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using Xunit;
 
@@ -6,6 +8,27 @@ namespace API.Tests.Core.Entities
 {
     public class VehicleEntityTests
     {
-       //Test cases for VehicalEntity
+        //Test cases for VehicalEntity
+        [Fact]
+        public void ValidateYearRange()
+        {
+            var vehicle = new Vehicle
+            {
+                Year = 3010,
+                Make = "Tesla",
+                Model = "Model 3"
+            };
+            Assert.True(ValidateModel(vehicle).Count > 0);
+        }
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
     }
+
+
 }
